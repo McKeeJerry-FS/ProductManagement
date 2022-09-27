5ff26588ac8565661c76c33795a36a6aaac31f60
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2022-09-26 21:55:37 -0400

    Add project files.

---
+++ src/ProductManagement.Domain/Data/ProductManagementDataSeedContributor.cs
@@ -0,0 +1,23 @@
+﻿using ProductManagement.Categories;
+using ProductManagement.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Data;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Repositories;
+
+namespace ProductManagement.Data
+{
+    public class ProductManagementDataSeedContributor : IDataSeedContributor, ITransientDependency
+    {
+        private readonly IRepository<Product, Guid> _productRepository;
+        private readonly IRepository<Category, Guid> _categoryRepository;
+        public async Task SeedAsync(DataSeedContext context)
+        {
+            /***** TODO: Seed initial data here *****/
+        }
+    }
+}
