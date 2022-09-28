7c6b085d5e41b42aa1f6bec64dcf31443a51cd49
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2022-09-27 23:31:25 -0400

    built out page

---
+++ src/ProductManagement.Web/Pages/Products/Index.cshtml.js
@@ -0,0 +1,41 @@
+﻿$(function () {
+    var l = abp.localization.getResource("ProductManagement");
+    var dataTable = $('#ProductsTable').DataTable(
+        abp.libs.dataTables.normalizeConfiguration({
+            serverSide: true,
+            paging: true,
+            order: [[0, "asc"]],
+            searching: false,
+            scrollX: true,
+            ajax: abp.libs.dataTables.createAjax(
+                productManagement.products.product.getList),
+            columnDefs: [
+                /* TODO: Column Definitions */
+                {
+                    title: l('Name'),
+                    data: "name"
+                },
+                {
+                    title: l('CategoryName'),
+                    data: "categoryName",
+                    orderable: false
+                },
+                {
+                    title: l('Price'),
+                    data: "price"
+                },
+                {
+                    title: l('StockState'),
+                    data: "stockState",
+                    render: function (data) {
+                        return l('Enum:StockState' + data);
+                    }
+                },
+                {
+                    title: l('CreationTime'),
+                    data: "creationTime",
+                    dataFormat: 'date'
+                },
+            ]
+        }));
+});
